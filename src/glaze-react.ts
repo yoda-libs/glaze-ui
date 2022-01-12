26d5866116c64213804cce64428ab0963e1d9b0b
Juliano Teixeira <jzucchetto@gmail.com>
2022-01-11 17:20:06 -0800

    fixed hmr

--- src/glaze-react.ts
+++ src/glaze-react.ts
@@ -1,7 +1,8 @@
-const mount = (component, react, reactDOM) => (container: HTMLElement, props) => {
+const mount = (component, react, reactDOM, hot) => (container: HTMLElement, props: any) => {
     const div = document.createElement('div');
     div.setAttribute('glaze', 'react-app-wrapper');
     container.appendChild(div);
+    if (hot) hot(props, div);
     const el = react.createElement(component, props);
     reactDOM.render(el, div);
   
@@ -13,9 +14,11 @@ const unmount = (reactDOM) => (container: HTMLElement, app: HTMLElement) => {
     container.removeChild(app);
 };
 
-export function glazeReact(component, react, reactDOM) {
+
+export function glazeReact(component, react, reactDOM, hot) {
     return { 
-        mount: mount(component, react, reactDOM), 
-        unmount: unmount(reactDOM) 
+        mount: mount(component, react, reactDOM, hot), 
+        unmount: unmount(reactDOM)
     }
 }
+
